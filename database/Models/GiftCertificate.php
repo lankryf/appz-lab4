b291fddf6d63202d94b02a7c7cff324e7bc5f7da
Oleksii Fedorenko <lankrf@gmail.com>
2025-04-22 22:33:23 +0300

    Init getters and setters

--- database/Models/GiftCertificate.php
+++ database/Models/GiftCertificate.php
@@ -7,4 +7,21 @@ use Bubblegum\Database\Model;
 class GiftCertificate extends Model
 {
     protected $tableName = 'gift_certificates';
+
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+    public function getCode(): string|null
+    {
+        return $this->code;
+    }
+    public function getUsed(): bool|null
+    {
+        return $this->used;
+    }
+    public function setUsed(bool $used): void
+    {
+        $this->used = (string)$used;
+    }
 }
--- database/Models/QuestRoom.php
+++ database/Models/QuestRoom.php
@@ -6,5 +6,32 @@ use Bubblegum\Database\Model;
 
 class QuestRoom extends Model
 {
-    protected $tableName = 'users';
+    protected $tableName = 'quest_rooms';
+
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+
+    public function getName(): string|null
+    {
+        return $this->name;
+    }
+    public function getPrice(): int|null
+    {
+        return $this->price;
+    }
+
+    public function getOpenHour(): int|null
+    {
+        return $this->open_hour;
+    }
+    public function getCloseHour(): int|null
+    {
+        return $this->close_hour;
+    }
+    public function getMaxPlayers(): int|null
+    {
+        return $this->max_players;
+    }
 }
